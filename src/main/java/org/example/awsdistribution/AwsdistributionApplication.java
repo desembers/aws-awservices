9c46386ebe8641c9b8f3ef2f2b2caa501992812d
Johs <206869862+desembers@users.noreply.github.com>
2025-11-06 18:44:17 +0900

    aws배포하기

--- src/main/java/org/example/awsdistribution/AwsdistributionApplication.java
+++ src/main/java/org/example/awsdistribution/AwsdistributionApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsdistributionApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(AwsdistributionApplication.class, args);
     }
