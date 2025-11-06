a44bb89186b6226e8b01cb9f70302eaa9be530b2
Johs <206869862+desembers@users.noreply.github.com>
2025-11-06 17:36:11 +0900

    awsdistribute

---
+++ src/main/java/org/example/awsdistribution/AwsdistributionApplication.java
@@ -0,0 +1,13 @@
+package org.example.awsdistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsdistributionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/awsdistribution/AwsdistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awsdistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsdistributionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
